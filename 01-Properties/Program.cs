f466079eab9d82dbd49606aff7de6c63ffb054d0
Damir Pasagic <damirpasa@gmail.com>
2020-09-29 13:56:17 +0200

    initial commit

---
+++ 01-Properties/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Properties
+{
+    class Account
+    {
+        public string Name { get; set; }
+        private decimal balance;
+
+        public Account(string accountName, decimal initialBalance)
+        {
+            Name = accountName;
+            Balance = initialBalance;
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+            private set
+            {
+                if (value > 0.0m)
+                {
+                    balance = value;
+                }
+            }
+        }
+        public void Deposit(decimal depositAmount)
+        {
+            if (depositAmount > 0.0m)
+            {
+                Balance = Balance + depositAmount;
+            }
+        }
+    }
+
+    class AccountTest
+    {
+        static void Main()
+        {
+            Account account1 = new Account("Damir Pasagic", 1000000.00m);
+            Account account2 = new Account("Rayan Pasagic", 10000000.00m);
+
+            Console.WriteLine($"{account1.Name}'s balance: {account1.Balance:C}");
+            Console.WriteLine($"{account2.Name}'s balance: {account2.Balance:C}");
+
+            Console.Write("\nEnter deposit amount for account1: ");
+            decimal depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"adding {depositAmount:C} to account1 balance\n");
+            account1.Deposit(depositAmount);
+
+            Console.WriteLine($"{account1.Name}'s balance: {account1.Balance:C}");
+            Console.WriteLine($"{account2.Name}'s balance: {account2.Balance:C}");
+
+            Console.Write("\nEnter deposit amount for account2: ");
+            depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"adding {depositAmount:C} to account2 balance\n");
+            account2.Deposit(depositAmount);
+
+            Console.WriteLine($"{account1.Name}'s balance: {account1.Balance:C}");
+            Console.WriteLine($"{account2.Name}'s balance: {account2.Balance:C}");
+
+            Console.Read();
+        } 
+    }
+}
---
+++ 03-Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(5));
+            Console.Read();
+        }
+
+        static long Factorial(long number)
+        {
+            if(number <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine($"Number {number}: {number * Factorial(number - 1)}");
+                return number * Factorial(number - 1);
+            }
+        }
+    }
+}
