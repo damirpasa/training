3fb186942db54b06980d55314eec3aef7b6b12ef
Damir Pasagic <damirpasa@gmail.com>
2020-09-29 15:18:13 +0200

    Collections implemented

--- 06-Collections/Program.cs
+++ 06-Collections/Program.cs
@@ -8,9 +8,60 @@ namespace _06_Collections
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("start");
+            // populate a List of strings
+            var items = new List<string>();
+            items.Add("aQua"); // add aQua to the end of the list
+            items.Add("RusT");
+            items.Add("yElLow");
+            items.Add("rEd");
+
+            // display initial List
+            Console.Write("items contains:");
+            foreach (var item in items)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.WriteLine(); // output end of line
+
+            // convert to uppercase, select those starting with "R" and sort
+            var startsWithR =
+                from item in items
+                let uppercaseString = item.ToUpper()
+                where uppercaseString.StartsWith("R")
+                orderby uppercaseString
+                select uppercaseString;
+
+            // display query results
+            Console.Write("results of query startsWithR:");
+            foreach(var item in startsWithR)
+            {
+                Console.Write($" {item}");
+            }
+            Console.WriteLine();
+
+            items.Add("rUbY");
+            items.Add("SaFfRon");
+
+            // display initial List
+            Console.Write("items contains:");
+            foreach(var item in items)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.WriteLine();
+
+            // display updated query results
+            Console.Write("results of query startsWithR:");
+            foreach(var item in startsWithR)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.WriteLine();
             Console.Read();
         }
     }
